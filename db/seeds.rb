b7c8240288879815dc2735b1d3534c00df2a1dca
Isabelle Brown <bellebrown619@gmail.com>
2023-03-21 10:59:18 -0400

    working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = []
+User.destroy_all
+5.times do
+  email = "#{Faker::Name.first_name}@hotmail.com"
+  user = User.new(password: "123456", email:email)
+  user.save!
+  users << user
+end
+
+Airplane.destroy_all
+
+Airplane.create!([{
+  model: "Soul",
+  class: "Pete Docter",
+  tailnumber: "",
+  home_airport: "1.week.ago"
+  minimum_hours: "After landing the gig of a lifetime, a New York jazz pianist suddenly finds himself trapped in a strange land between Earth and the afterlife.",
+  required_licenses: "1.week.ago"
+  user_id: users.sample.id
+  latitude: "Pete Docter",
+  longitude: "After landing the gig of a lifetime, a New York jazz pianist suddenly finds himself trapped in a strange land between Earth and the afterlife.",
+  description: "1.week.ago"
+},
+{
