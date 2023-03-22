a6c6164c795c9241b47a3c2efa95e76db6a05d2b
Juan Catera <juancito_tds@hotmail.com>
2023-03-22 11:09:07 -0300

    Working on reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @airplane = Airplane.find(params[:list_id])
+    @review.airplane = @airplane
+    if @review.save
+      redirect_to airplane_path(@airplane)
+    else
+      # @bookmark = Bookmark.new
+      # render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to airplane_path(@review.ariplane)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :score)
+  end
 end
