6d7c6fc976b18f774b82353c541ef9693fcb9882
Juan Catera <juancito_tds@hotmail.com>
2023-03-27 14:39:41 -0300

    Pull

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import AddressAutocompleteController from "./address_autocomplete_controller"
 application.register("address-autocomplete", AddressAutocompleteController)
 
+import CityAutocompleteController from "./city_autocomplete_controller"
+application.register("city-autocomplete", CityAutocompleteController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
